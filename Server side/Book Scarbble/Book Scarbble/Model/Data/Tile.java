29b7aecced69aa536020558b04ffe7c59409c57b
RoeeShemesh <132275088+RoeeShemesh@users.noreply.github.com>
2023-05-07 21:38:25 +0300

    Update Tile.java

--- Server side/Book Scarbble/Book Scarbble/Model/Data/Tile.java
+++ Server side/Book Scarbble/Book Scarbble/Model/Data/Tile.java
@@ -1,7 +1,4 @@
-//Written by: Roee Shemesh
-//I.D: 209035179
 package test;
-
 import java.util.Objects;
 import java.util.Random;
 
@@ -28,9 +25,13 @@ public class Tile {
     }
 
     public static class Bag {
+        //The amount of tiles in the sack for each letter at the start of a game.
         private final int[] startAmountArray = {9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1};
+        //The current amount of tiles in the bag for each letter.
         private final int[] amountLetterArray = {9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1};
         private final Tile[] tileArray=new Tile[26];
+        
+        //Return a random tile from the sack or null.
         public Tile getRand() {
             if (size() == 0)
                 return null;
@@ -41,6 +42,7 @@ public class Tile {
             return tileArray[rndIndex];
         }
 
+        //Given a letter, returns his tile from the sack or null.
         public Tile getTile(char letter) {
             int index = letter - 'A';
             if(index<0||index> amountLetterArray.length-1)
@@ -51,6 +53,7 @@ public class Tile {
             return tileArray[index];
         }
 
+        //Given a tile, returns it to the bag and limits the amount according to the rules of the game.
         public void put(Tile tile) {
             int index = tile.letter - 'A';
             if(amountLetterArray[index]==startAmountArray[index])
@@ -58,6 +61,7 @@ public class Tile {
             amountLetterArray[index]++;
         }
 
+        //Return the current total of tiles in the bag.
         public int size() {
             int tilesInBag = 0;
             for (int i : amountLetterArray)
@@ -65,19 +69,21 @@ public class Tile {
             return tilesInBag;
         }
 
+        //Return the currnet amount of each tile.
         public int[] getQuantities() {
             return amountLetterArray.clone();
         }
 
         private Bag() {
             char[] letterArray = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+            //Score of each letter
             int[] scoreArray = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
             for (int i = 0; i< tileArray.length; i++)
                 tileArray[i]=new Tile(letterArray[i], scoreArray[i]);
         }
 
+        //Singlton - Bag
         private static Bag myBag = null;
-
         public static Bag getBag() {
             if (myBag == null)
                 myBag = new Bag();
