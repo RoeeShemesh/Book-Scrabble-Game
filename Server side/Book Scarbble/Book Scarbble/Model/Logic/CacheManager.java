5565d9a214e7edd8ed90a9002ec078531245e8db
RoeeShemesh <132275088+RoeeShemesh@users.noreply.github.com>
2023-05-08 09:52:40 +0300

    Update CacheManager.java

--- Server side/Book Scarbble/Book Scarbble/Model/Logic/CacheManager.java
+++ Server side/Book Scarbble/Book Scarbble/Model/Logic/CacheManager.java
@@ -1,18 +1,24 @@
-//written by: Roee Shemesh - 209035179
 package test;
-
 import java.util.HashSet;
+
 public class CacheManager {
+    //LRU or LFU.
     private final CacheReplacementPolicy crp;
+    //The maximum size od the cache.
     private final int size;
-	public CacheManager(int size,CacheReplacementPolicy crp){
+    HashSet<String> wordsInCache=new HashSet<String>();
+
+    public CacheManager(int size,CacheReplacementPolicy crp){
         this.size=size;
         this.crp=crp;
     }
-    HashSet<String> wordsInCache=new HashSet<String>();
+
+    //Returns true if the word is inside the cache, if not return false.
     public boolean query(String word){
         return wordsInCache.contains(word);
     }
+	
+    //Given word, updates the crp, add it to the cache, remove other word from cache when needed.
     public void add(String word){
         crp.add(word);
         wordsInCache.add(word);
